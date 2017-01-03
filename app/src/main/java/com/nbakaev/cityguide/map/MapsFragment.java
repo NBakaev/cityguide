7c2997859f092a425080ca20664e4649cb7c9a04
Nikita Bakaev <ya@nbakaev.ru>
2017-01-03 11:54:12 +0300

    experimental feature

--- app/src/main/java/com/nbakaev/cityguide/map/MapsFragment.java
+++ app/src/main/java/com/nbakaev/cityguide/map/MapsFragment.java
@@ -3,6 +3,7 @@ package com.nbakaev.cityguide.map;
 import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.TabLayout;
@@ -53,6 +54,8 @@ import io.reactivex.schedulers.Schedulers;
 
 import static com.nbakaev.cityguide.poi.PoiProvider.DISTANCE_POI_DOWNLOAD;
 import static com.nbakaev.cityguide.poi.PoiProvider.DISTANCE_POI_DOWNLOAD_MOVE_CAMERA_REFRESH;
+import static com.nbakaev.cityguide.ui.AnimationUtils.collapse;
+import static com.nbakaev.cityguide.ui.AnimationUtils.expand;
 
 public class MapsFragment extends BaseFragment implements OnMapReadyCallback, GoogleMap.OnMapLoadedCallback {
 
@@ -211,7 +214,34 @@ public class MapsFragment extends BaseFragment implements OnMapReadyCallback, Go
 
         bottomSheet.setVisibility(View.INVISIBLE);
         mBottomSheetBehavior.setPeekHeight(DEFAULT_BOTTOM_SHEET_HEIGHT);
+
+        if (settingsService.getSettings().isEnableExperimentalFeature()) {
+            mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+                @Override
+                public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                    switch (newState) {
+                        case BottomSheetBehavior.STATE_HIDDEN:
+                            expand(baseActivity.toolbar);
+                            break;
+
+                        case BottomSheetBehavior.STATE_COLLAPSED:
+                            expand(baseActivity.toolbar);
+                            break;
+
+                        case BottomSheetBehavior.STATE_EXPANDED:
+                            collapse(baseActivity.toolbar);
+                            break;
+                    }
+                }
+
+                @Override
+                public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+                }
+            });
+        }
     }
+
 
     @Override
     public void onMapLoaded() {
---
+++ app/src/main/java/com/nbakaev/cityguide/ui/AnimationUtils.java
@@ -0,0 +1,60 @@
+package com.nbakaev.cityguide.ui;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+/**
+ * Created by ya on 1/3/2017.
+ */
+
+public class AnimationUtils {
+
+    public static void expand(final View v) {
+        v.measure(ViewPager.LayoutParams.MATCH_PARENT, ViewPager.LayoutParams.WRAP_CONTENT);
+        final int targtetHeight = v.getMeasuredHeight();
+        if (v.isShown()) {
+        } else {
+            v.getLayoutParams().height = 0;
+            v.setVisibility(View.VISIBLE);
+            Animation a = new Animation() {
+                @Override
+                protected void applyTransformation(float interpolatedTime, Transformation t) {
+                    v.getLayoutParams().height = interpolatedTime == 1 ? ViewPager.LayoutParams.WRAP_CONTENT : (int) (targtetHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+
+                @Override
+                public boolean willChangeBounds() {
+                    return true;
+                }
+            };
+            a.setDuration(targtetHeight + 100);
+            v.startAnimation(a);
+        }
+    }
+
+    public static void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    v.setVisibility(View.GONE);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        a.setDuration(v.getLayoutParams().height + 100);
+        v.startAnimation(a);
+    }
+}
