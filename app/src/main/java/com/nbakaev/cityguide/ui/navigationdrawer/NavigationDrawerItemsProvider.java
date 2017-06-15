7a7297413c1e6f50d0e3acc93e5bcbf8435f50a1
Nikita Bakaev <ya@nbakaev.ru>
2017-06-15 08:53:03 +0300

    delete unused code + add icons

--- app/src/main/java/com/nbakaev/cityguide/ui/navigationdrawer/NavigationDrawerItemsProvider.java
+++ app/src/main/java/com/nbakaev/cityguide/ui/navigationdrawer/NavigationDrawerItemsProvider.java
@@ -17,7 +17,7 @@ public class NavigationDrawerItemsProvider {
         dataList.add(new NavigationDrawerItem(R.string.drawer_map, R.drawable.ic_place_black_24dp));
         dataList.add(new NavigationDrawerItem(R.string.drawer_near_me, R.drawable.ic_near_me_black_24dp));
         dataList.add(new NavigationDrawerItem(R.string.drawer_cities, R.drawable.ic_business_black_24dp));
-        dataList.add(new NavigationDrawerItem(R.string.drawer_trips, R.drawable.ic_local_taxi_black_24dp));
+//        dataList.add(new NavigationDrawerItem(R.string.drawer_trips, R.drawable.ic_local_taxi_black_24dp));
         dataList.add(new NavigationDrawerItem(R.string.drawer_auth, R.drawable.ic_account_circle_black_24dp));
         dataList.add(new NavigationDrawerItem(R.string.drawer_about, R.drawable.ic_build_black_24dp));
         dataList.add(new NavigationDrawerItem(R.string.drawer_qr, R.drawable.ic_camera_black_24dp));
